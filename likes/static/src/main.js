3aafd95a901de90581a4fd27020ea9173fa76148
Aleksandr Kazimir <Kazimir.Aleksandr@svyaznoy.ru>
2020-03-14 15:19:31 +0300

    feat(metrika): add to likes and vuemask

--- likes/static/src/main.js
+++ likes/static/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+if (process.env.NODE_ENV === 'production') {
+  import('./metrika.js');
+}
+
 Vue.config.productionTip = false
 
 new Vue({
--- vuemask/src/main.js
+++ vuemask/src/main.js
@@ -3,6 +3,10 @@ import VueMask from '@yasanchezz/vue-mask';
 import '@yasanchezz/vue-mask/dist/vue-mask.css';
 import App from './App.vue';
 
+if (process.env.NODE_ENV === 'production') {
+  import('./metrika.js');
+}
+
 Vue.use(VueMask);
 
 Vue.config.productionTip = false;
